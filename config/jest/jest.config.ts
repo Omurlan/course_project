4501e3c54a522cfc10c02e57e9a9a50beebc87b8
Omurlan <abdrashevomurlan@gmail.com>
2023-09-30 01:28:59 +0600

    Удаление виртуализации, скрипт для автоматизации исправления импортов ts-morph,

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -40,6 +40,7 @@ export default {
   ],
   setupFilesAfterEnv: ['<rootDir>/config/jest/setupTests.ts'],
   moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
     '\\.s?css$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
         '<rootDir>config/jest/mocks/image.ts',
